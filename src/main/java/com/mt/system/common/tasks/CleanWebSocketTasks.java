b77d81b66a4ed2c5c15f5f3a0559612108137770
chenpan <2923324948@qq.com>
2019-04-11 11:00:22 +0800

    cp

--- src/main/java/com/mt/system/common/tasks/CleanWebSocketTasks.java
+++ src/main/java/com/mt/system/common/tasks/CleanWebSocketTasks.java
@@ -4,6 +4,8 @@ import com.mt.system.common.util.DateUtils;
 import com.mt.system.domain.constant.ConnectTimeConstant;
 import com.mt.system.domain.entity.MtSession;
 import com.mt.system.websocket.MtWebSocketServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -21,11 +23,12 @@ import java.util.concurrent.ConcurrentHashMap;
 @Configurable
 @EnableScheduling
 public class CleanWebSocketTasks {
-
+    private static Logger logger = LoggerFactory.getLogger(MtWebSocketServer.class);
     //每10分钟执行一次
 //    @Scheduled(cron = "0 */1 *  * * * ")
     @Scheduled(cron = "0 */10 * * * * ")
     public void reportCurrentByCron(){
+        logger.info("执行定时任务");
         ConcurrentHashMap<String,MtSession> mtSessionMap = MtWebSocketServer.getMtSessionMap();
         for (MtSession mtSession:mtSessionMap.values()){
             if(DateUtils.currentCompare(mtSession.getConnectTime())>ConnectTimeConstant.EFFECTIVE_TIME_CODE){
